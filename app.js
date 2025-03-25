57f64d9f19c3b3d77990c8978f8ba87d022a0387
SergioCaMi <sergiocami84@gmail.com>
2025-03-25 14:06:35 +0100

    fin ejercicio

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // Buena suerte ! Espero que se te encianda la "bombilla"!
+const bulb = document.getElementById("bulb");
+
+
+// Opción 1
+// bulb.addEventListener("click", event =>{
+//     if (bulb.classList.contains("bulb-off")){
+//         bulb.classList.remove("bulb-off");
+//         bulb.classList.add("bulb-on");
+//     } else {
+//         bulb.classList.remove("bulb-on");
+//         bulb.classList.add("bulb-off");
+//     }
+// });
+
+
+// Opción 2
+bulb.addEventListener("mouseover", (event) => {
+  bulb.classList.remove("bulb-off");
+  bulb.classList.add("bulb-on");
+});
+
+bulb.addEventListener("mouseout", (event) => {
+    bulb.classList.remove("bulb-on");
+    bulb.classList.add("bulb-off");
+  });
